7a7dc19ea48980bd6ac38528a7c77783c3b112f6
Angshuman Paul <paulang1807@gmail.com>
2020-05-18 09:57:56 -0700

    fix quickbase sample query issues

--- python/quickbase_samples.py
+++ python/quickbase_samples.py
@@ -17,8 +17,9 @@ appToken = 'appToken'
 # Query a table
 # clist is a list of quickbase field ids
 metricQry= "https://domain.quickbase.com/db/{0}?a=API_GenResultsTable&".format(dbID)
-metricQry += "usertoken={}&apptoken={}}&".format(userToken, appToken) \
-            "clist=6.144.146&slist=6&options=csv"
+metricQry += "usertoken={}&apptoken={}}&".format(userToken, appToken)
+metricQry += "clist=6.144.146&slist=6&options=csv"
+
 response=requests.get(metricQry).content
 df1=pd.read_csv(io.StringIO(response.decode('ISO-8859-1')))
 
@@ -28,8 +29,8 @@ df = pd.DataFrame()
 for indx, row in df.iterrows():
     valToInsert = row['colName']
     metricQry= "https://domain.quickbase.com/db/{}?a=API_AddRecord&".format(dbID)
-    metricQry += "usertoken={}&apptoken={}}&".format(userToken, appToken) \
-                    "_fid_6={}".format(valToInsert)
+    metricQry += "usertoken={}&apptoken={}}&".format(userToken, appToken)
+    metricQry += "_fid_6={}".format(valToInsert)
     requests.get(metricQry).content
 
 # Query table so that resultset includes record id and update id
@@ -38,8 +39,8 @@ filter1='filter1'
 filter2='filter2'
 
 metricQry= "https://domain.quickbase.com/db/{0}?a=API_DoQuery&includeRids=1&".format(dbID)
-metricQry += "usertoken={}&apptoken={}}&".format(userToken, appToken) \
-            "query={'22'.CT." + str(filter1) + "}AND{'38'.CT." + filter2 + "}&clist=148&slist=3"
+metricQry += "usertoken={}&apptoken={}}&".format(userToken, appToken)
+metricQry += "query={'22'.CT." + str(filter1) + "}AND{'38'.CT." + filter2 + "}&clist=148&slist=3"
 response=requests.get(metricQry).content
 
 # Extract record id and update id from the resultset
@@ -52,6 +53,6 @@ for child in list(xtree):
 # Update a table by field id
 updVal='updVal'
 metricQry= "https://domain.quickbase.com/db/{0}?a=API_EditRecord&rid={1}&".format(dbID, rec_id)
-metricQry += "usertoken={}&apptoken={}}&".format(userToken, appToken) \
-            "_fid_14={}&update_id={}".format(updVal, update_id)
+metricQry += "usertoken={}&apptoken={}}&".format(userToken, appToken)
+metricQry += "_fid_14={}&update_id={}".format(updVal, update_id)
 requests.get(metricQry).content
