8041fa19b8c7612dc6e46801ad6a4b3b5c546e80
Angshuman Paul <paulang1807@gmail.com>
2025-06-22 22:24:48 -0700

    add comments to package init

---
+++ code-samples/PackageBoilerplate/MainPackage/__init__.py
@@ -0,0 +1,5 @@
+'''
+__init__.py is a special file used in Python to define packages and initialize their namespaces
+This file is executed when the package is imported, allowing you to set up package-level variables, import submodules, or perform initialization tasks.
+In this case, it is used to define the package's name and version.
+'''
